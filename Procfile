ea59d5b966623daa0f50ee667351da691da1797a
Sharmila-86 <67464996+Sharmila-86@users.noreply.github.com>
2020-08-06 17:48:00 +0530

    Add files via upload

--- Procfile
+++ Procfile
@@ -1,8 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 10 21:56:38 2020
-
-@author: sharm
-"""
-
-web: gunicorn knn:app
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -1,28 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 10 21:47:42 2020
-
-@author: sharm
-"""
-
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/predict',methods=['POST'])
 def predict():
     '''
-      #  For rendering results on HTML GUI
+    For rendering results on HTML GUI
     '''
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
-    
-    
-    return render_template('index.html', prediction_text='THIS CASE IS DIAGONISED AS $ {}'.format(output))
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
